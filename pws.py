e3dffa215a18d52811a19902307d4c5775188dea
Tim Kolijn <tim@tim-nl.nl>
2024-01-22 21:45:07 +0100

    Update pws.py

--- pws.py
+++ pws.py
@@ -9,7 +9,7 @@ import sys
 class World:
 
     def __init__(self):
-        self.world_array = np.zeros((50, 50, 100), np.int8)
+        self.world_array = np.zeros((160, 160, 160), np.int8)
 
     def setBlock(self, x, y, z, t):
         self.world_array[x][z][y] = t
